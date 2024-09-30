6a0c1cd4a23ef44026a87918955655a1dec4ec4e
Dave J <29786887+daveswork@users.noreply.github.com>
2024-09-30 15:25:14 -0400

    Added some client-side code.

--- server/src/seed.py
+++ server/src/seed.py
@@ -15,23 +15,32 @@ def run():
     fake = Faker()
 
     users = []
-    things = ['comb', 'beanie', 'hacky sack', 'soap', 'chair', 'pillow']
+    things = [{'name':'comb', 'link':'https://i.imgur.com/jIN9hIA.jpg'}, 
+              {'name':'beanie', 'link':'https://i.imgur.com/nGwLAH4.jpg'}, 
+              {'name':'hacky sack', 'link':'https://i.imgur.com/w2z4bpq.jpg'}, 
+              {'name':'soap', 'link':'https://i.imgur.com/EMbSFQZ.jpg'}, 
+              {'name':'chair', 'link':'https://i.imgur.com/SrmeCdX.jpg'}, 
+              {'name':'pillow', 'link':'https://i.imgur.com/LDXYi89.jpg'}, 
+              {'name':'t-shirt', 'link':'https://i.imgur.com/P2PlMUU.jpg'}, 
+              {'name':'pot', 'link':'https://i.imgur.com/sIudgDS.jpg'}]
     items = []
     for i in range(4):
         u = User(
-            username=fake.user_name(),
             firstname=fake.first_name_nonbinary(),
-            lastname = fake.last_name_nonbinary()
+            lastname = fake.last_name_nonbinary(),
+            username=fake.user_name()
         )
         users.append(u)
 
-
+    for i in range(len(things)):
         item = Item(
-            name = things[i],
+            name = things[i]['name'],
             description = fake.sentence(),
             qty = randint(1, 30),
+            item_image = things[i]['link'],
             price = randint(5, 50)
         )
+        print(item)
         items.append(item)
 
     # commit changes
