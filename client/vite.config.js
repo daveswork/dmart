f7c5b4bf34f014a5ce4b72cad00c25401d332c8b
Dave J <29786887+daveswork@users.noreply.github.com>
2024-10-22 16:17:54 -0400

    FE: Added certs and https

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from 'fs'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
+    https:{
+      key: fs.readFileSync(path.resolve(__dirname, 'certs/localhost-key.pem')),
+      cert: fs.readFileSync(path.resolve(__dirname, 'certs/localhost.pem'))
+    },
     port:5173, 
     proxy: {
       '/api': {
